61a5dae07f38824049a0523c9023b534ce6e9507
EmilioBlacksmith <emilioblacksmithlush@gmail.com>
2025-01-08 20:49:24 -0600

    fix: refactor blog links and overhaul

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-	"emilioherrera.dev | Software Engineer, Web Developer, and Blogger";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Software Engineer, Web Developer, and Blogger";
+export const SITE_DESCRIPTION = "Welcome to my personal blog!";
